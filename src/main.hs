c8e28b3f1a13cee91c3da2df12083c18461c862b
Harry Garrood <hdgarrood@gmail.com>
2013-11-20 05:05:18 +0000

    slightly more helpful

--- src/main.hs
+++ src/main.hs
@@ -4,6 +4,9 @@ import Data.Complex
 import Codec.Picture
 import Control.Monad
 import Text.Printf
+import Control.Concurrent
+import System.Directory
+import System.Exit
 
 import Rendering
 import Magnifying
@@ -11,21 +14,62 @@ import Fractals
 import Vec2
 
 main :: IO ()
-main = forM_ (zip [0,1..] images) $ (\(x, img) -> do
-    writePng (nameFrom x) img
-    showProgress x)
+main = do
+    createOutputDir
+    cleanOutputDir
+    mapM_ writeToDisk (zip [0,1..] images)
     where
-        nameFrom :: Int -> String
-        nameFrom x = "output/frame" ++ (printf "%05d" x) ++ ".png"
+        outputDir = "output"
 
-        images = take numFrames $ magnifiedRender
-                                    (320, 240)
-                                    wholeMandelbrot
-                                    target
-                                    (mandelbrot 1000)
-        target = (-0.7321919) :+ 0.2326958
-        numFrames = 100
+        nameFrom :: Int -> String
+        nameFrom x = outputDir ++ "/frame" ++ (printf "%05d" x) ++ ".png"
+
+        numFrames :: Int
+        numFrames = 200
+
+        target = (-0.743643884) :+ 0.131825909
+        images = take numFrames $
+            magnifiedRender
+                (320, 240)
+                wholeMandelbrot
+                target
+                (mandelbrot 2000)
 
         showProgress :: Int -> IO ()
         showProgress x = putStrLn $
             printf "Rendering (%d / %d) ...    " x numFrames
+
+        writeToDisk :: (Int, Image PixelRGB8) -> IO ()
+        writeToDisk (x, img) = writePng (nameFrom x) img
+
+        createOutputDir = createDirectoryIfMissing True outputDir
+
+        getDirectoryContents' =
+            liftM (filter (not . (`elem` [".", ".."]))) .
+            getDirectoryContents
+
+        cleanOutputDir = do
+            fs <- getDirectoryContents' outputDir
+            let shouldAsk = length fs > 0
+
+            ok <- if shouldAsk
+                then confirm $ "delete " ++ show (length fs) ++
+                          " files in ./output/ ?"
+                else return True
+
+            if ok
+                then do
+                    removeDirectoryRecursive outputDir
+                    createOutputDir
+                else do
+                    putStrLn "doing nothing."
+                    exitWith (ExitFailure 1)
+
+        confirm :: String -> IO Bool
+        confirm question = do
+            putStrLn $ question ++ " [y/n]"
+            ch <- getChar
+            return $ case ch of
+                'y' -> True
+                'Y' -> True
+                _   -> False
