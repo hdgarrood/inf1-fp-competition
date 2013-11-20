b0cc6b56262c1ff6fe64330b0d60f6b974112ee9
Harry Garrood <hdgarrood@gmail.com>
2013-11-20 16:24:50 +0000

    fix ignores

--- src/main.hs
+++ src/main.hs
@@ -1,11 +1,15 @@
 module Main where
 
 import Data.Complex
+import Data.Time.Clock
+import Data.Time.Format
+import System.Locale
 import Codec.Picture
 import Control.Monad
 import Text.Printf
 import Control.Concurrent
 import Control.Parallel.Strategies
+import System.FilePath
 import System.Directory
 import System.Exit
 
@@ -14,21 +18,26 @@ import Magnifying
 import Fractals
 import Vec2
 
+-- Rendering options
+numFrames :: Int
+numFrames = 20
+
+-- the complex number to zoom in on
+target :: C
+target = (-0.743643884) :+ 0.131825909
+
 main :: IO ()
 main = do
-    createOutputDir
-    cleanOutputDir
-    mapM_ writeToDisk (zip [0,1..] images)
+    dir <- getOutputDir
+    prepareOutputDir dir
+    mapM_ (writeToDisk dir) (zip [0,1..] images')
     where
-        outputDir = "output"
+        getOutputDir :: IO FilePath
+        getOutputDir = fmap (formatTime defaultTimeLocale "output/%y%m%d-%H%M%S")
+                        getCurrentTime
 
-        nameFrom :: Int -> String
-        nameFrom x = outputDir ++ "/frame" ++ (printf "%05d" x) ++ ".png"
-
-        numFrames :: Int
-        numFrames = 200
-
-        target = (-0.743643884) :+ 0.131825909
+        frameName :: Int -> FilePath
+        frameName x = "frame" ++ (printf "%05d" x) ++ ".png"
 
         images :: [Image PixelRGB8]
         images = take numFrames $
@@ -38,42 +47,24 @@ main = do
                 target
                 (mandelbrot 2000)
 
+        -- Same as images, but evaluated in parallel
         images' :: [Image PixelRGB8]
-        images' = images `using` (parMap id)
+        images' = images `using` (parList rdeepseq)
 
         showProgress :: Int -> IO ()
         showProgress x = putStrLn $
             printf "Rendering (%d / %d) ...    " x numFrames
 
-        writeToDisk :: (Int, Image PixelRGB8) -> IO ()
-        writeToDisk (x, img) = writePng (nameFrom x) img
+        writeToDisk :: FilePath -> (Int, Image PixelRGB8) -> IO ()
+        writeToDisk outputDir (x, img) = do
+            writePng (outputDir </> frameName x) img
 
-        createOutputDir = createDirectoryIfMissing True outputDir
-
-        getDirectoryContents' =
-            liftM (filter (not . (`elem` [".", ".."]))) .
-            getDirectoryContents
-
-        cleanOutputDir = do
-            fs <- getDirectoryContents' outputDir
-            let shouldAsk = length fs > 0
-
-            ok <- if shouldAsk
-                then confirm $ "delete " ++ show (length fs) ++
-                          " files in ./output/ ?"
-                else return True
-
-            if ok
-                then do
-                    removeDirectoryRecursive outputDir
-                    createOutputDir
-                else do
-                    putStrLn "doing nothing."
-                    exitWith (ExitFailure 1)
+        prepareOutputDir :: FilePath -> IO ()
+        prepareOutputDir = createDirectory
 
         confirm :: String -> IO Bool
         confirm question = do
-            putStrLn $ question ++ " [y/n]"
+            putStr $ question ++ " [y/n] "
             ch <- getChar
             return $ case ch of
                 'y' -> True
